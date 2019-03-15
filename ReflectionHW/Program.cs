2841b783859024f1df8a1f02fd507b7f9fc429fe
Aseka <grooves97@gmail.com>
2019-03-15 15:25:44 +0600

    Fatality

---
+++ ReflectionHW/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1");
+            Type typeConsole = typeof(Console);
+            MethodInfo[] methodConsole = typeConsole.GetMethods();
+
+            foreach (MethodInfo mi in methodConsole)
+            {
+                Console.WriteLine($"{mi.Name}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TASK 2");
+
+            User user = new User("Aslan","Nurseitov",123456,"Lenina");
+            Type typeUser = typeof(User);
+
+            foreach (PropertyInfo property in typeUser.GetProperties())
+            {
+                Console.WriteLine($"{property.Name}={property.GetValue(user,null)}");
+            }
+
+        }
+    }
+}
---
+++ ReflectionHW/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionHW
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+
+        public User(string name, string lastname,int phone,string address)
+        {
+            Name = name;
+            Lastname = lastname;
+            Phone = phone;
+            Address = address;
+        }
+
+
+    }
+}
